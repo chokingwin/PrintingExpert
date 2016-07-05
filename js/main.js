797125e472b42ae21eecb41963dc28d2cec23e3a
chokingwin <2205405321@qq.com>
2016-07-05 11:53:39 +0800

    完善计价 content.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 /**
  * Created by chokingwin on 2016-07-04.
  */
+var onePContent = {
+    top:0,
+    bottom:1
+};
 
 function SItemClick(obj) {
     var parobj = obj.parentNode.parentNode; //ul
@@ -13,12 +17,41 @@ function SItemClick(obj) {
 
     var name = obj.attributes['name'].value;
     var val = obj.attributes['val'].value;
-    switch (name){
-        case 'qrCode':
-            $('.qrCode').hide();
-            $('.qrCode').eq(val).show();
-            break;
+
+    $('.qrCode').hide();
+    $('.qrCode').eq(val).show();
+}
+function SItemClickDouble(obj) {
+    var parobj = obj.parentNode.parentNode; //ul
+    var pars = parobj.children;            //li
+    for (var i = 0; i < pars.length; i++) {
+        pars[i].firstChild.className = "";
     }
+    obj.className = "selected";
+    obj.blur();
+    var name = obj.attributes['name'].value;
+    var val = obj.attributes['val'].value;
+    onePContent[name] = val;
+
+    $('.calc .content').hide();
+    if(onePContent.top == 0 && onePContent.bottom == 0){
+        $('.calc .content').eq(0).show();
+    }else if(onePContent.top == 0 && onePContent.bottom == 1){
+        $('.calc .content').eq(1).show();
+    }else if(onePContent.top == 1 && onePContent.bottom == 0){
+        $('.calc .content').eq(2).show();
+    }else{
+        $('.calc .content').eq(3).show();
+    }
+}
+
+function calc(obj){
+    var price = obj.attributes['val'].value;
+    var inputTag = obj.parentNode.children[0].children[1];
+    var allPriceTag = obj.parentNode.children[1];
+    var allPrice = inputTag.value * parseFloat(price);
+    allPriceTag.innerHTML = allPrice;
+    console.log(input.tagName);
 }
 
 $('.contactBar').mouseenter(function(){
@@ -27,9 +60,6 @@ $('.contactBar').mouseenter(function(){
     },'700');
 });
 $('.contactBar').mouseleave(function(){
-    /*$(this).css({
-     'top': '20px'
-     });*/
     $(this).animate({
         'top': '-255px'
     },'700');
